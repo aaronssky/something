0d125a5dedf68455ec21d0f452052c941a84ddf2
chenjunlun <aaa>
2019-03-23 16:27:10 +0800

    update:提交dist目录

---
+++ dist/precache-manifest.a605dda6667c0c1095edca97fc955941.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "54185c6f43216f20d1a7595e32a27f97",
+    "url": "index.html"
+  },
+  {
+    "revision": "cff146319bc5d8842bfc",
+    "url": "assets/js/product.ca067133.js"
+  },
+  {
+    "revision": "e750f8f4cb730acfae8f",
+    "url": "assets/js/chunk-vendors.43879df3.js"
+  },
+  {
+    "revision": "9103ac4ae0ea587d8d62",
+    "url": "assets/js/chunk-2f2688e6.90b7e472.js"
+  },
+  {
+    "revision": "38c1ae00cf5faf511a1e",
+    "url": "assets/js/app.5f438587.js"
+  },
+  {
+    "revision": "ee3500dd691560271320",
+    "url": "assets/js/about.b5e4f252.js"
+  }
+];
