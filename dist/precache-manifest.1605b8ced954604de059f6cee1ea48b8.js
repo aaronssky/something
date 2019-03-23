99e9c79d73c286481306084357113961898acad6
chenjunlun <aaa>
2019-03-23 16:19:26 +0800

    update:提交dist目录

---
+++ dist/precache-manifest.1605b8ced954604de059f6cee1ea48b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "ec28631c889602ed612afaeed5ad7e5a",
+    "url": "index.html"
+  },
+  {
+    "revision": "cff146319bc5d8842bfc",
+    "url": "assets/js/product.ca067133.js"
+  },
+  {
+    "revision": "b8ac05b7fdf00181bdb1",
+    "url": "assets/js/chunk-vendors.0e46da7a.js"
+  },
+  {
+    "revision": "9103ac4ae0ea587d8d62",
+    "url": "assets/js/chunk-2f2688e6.90b7e472.js"
+  },
+  {
+    "revision": "fdfcd7a7a00a897180ad",
+    "url": "assets/js/app.3e97ce7b.js"
+  },
+  {
+    "revision": "ee3500dd691560271320",
+    "url": "assets/js/about.b5e4f252.js"
+  }
+];
